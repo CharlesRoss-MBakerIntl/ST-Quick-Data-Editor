165e1d7567f0e1cc55c3a0922892b539b911440b
Charles Ross <charles.ross@mbakerintl.com>
2024-05-01 12:39:54 -0800

    fix points

--- data.py
+++ data.py
@@ -3,7 +3,7 @@ class Sources:
     def __init__(self):
 
         self.polygons = "https://services.arcgis.com/r4A0V7UzH9fcLVvv/arcgis/rest/services/CY2024_Project_Polygons/FeatureServer"
-        self.points = "https://services.arcgis.com/r4A0V7UzH9fcLVvv/arcgis/rest/services/CY2024_Project_Polygons/FeatureServer"
+        self.points = "https://services.arcgis.com/r4A0V7UzH9fcLVvv/arcgis/rest/services/CY2024_Project_Points/FeatureServer"
         self.starts = "https://services.arcgis.com/r4A0V7UzH9fcLVvv/arcgis/rest/services/CY2024_Project_Start_Points/FeatureServer"
         self.ends = "https://services.arcgis.com/r4A0V7UzH9fcLVvv/arcgis/rest/services/CY2024_Project_End_Points/FeatureServer"
 
